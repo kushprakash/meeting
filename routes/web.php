ada71922c9f342961ee03e2b4265784138d09061
Kush Prakash <63534055+kushprakash@users.noreply.github.com>
2026-09-24 19:19:38 +0530

    support deploy via /deploy, /deploy.php and /?token=...

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 
+function runDeploymentScript() {
+    $fileInPublic = public_path('deploy.php');
+    $fileInRoot = base_path('deploy.php');
+    if (file_exists($fileInPublic)) {
+        require $fileInPublic;
+        exit;
+    } elseif (file_exists($fileInRoot)) {
+        require $fileInRoot;
+        exit;
+    }
+}
+
 Route::get('/', function () {
+    if (request()->has('token')) {
+        runDeploymentScript();
+    }
     return view('welcome');
+});
+
+Route::get('/deploy', function () {
+    runDeploymentScript();
+    abort(404);
+});
+
+Route::get('/deploy.php', function () {
+    runDeploymentScript();
+    abort(404);
 });
 
 Route::get('/auth/{provider}/redirect', [App\Http\Controllers\Api\V1\SocialAuthController::class, 'redirectToProvider']);
